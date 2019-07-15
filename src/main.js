dcb5a4d16e8025632a03be7fe5cd5a1c1a4957fd
cyy <1689456277@qq.com>
2019-07-15 17:57:09 +0800

    main引入element

--- src/main.js
+++ src/main.js
@@ -5,8 +5,16 @@ import App from './App'
 import router from './router'
 import baseComponent from "@/base/index"
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' 
+import "@/common/utils/filter"
+import axios from 'axios'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 Vue.use(baseComponent)
+
+Vue.prototype.$axios = axios
 
 /* eslint-disable no-new */
 new Vue({
